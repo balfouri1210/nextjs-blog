ffffa5f0f6b4c5de02043f1aa8e24c77c337f2d8
jonghoon kim <balfouri1210@gmail.com>
2021-10-25 02:01:43 +0900

    Refactor: convert language from JS to TS

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,8 +1,15 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostsIds, getPostData } from '../../lib/posts';
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export default function Post({ postData }) {
+export default function Post({ postData }: {
+  postData: {
+    id: String,
+    title: String,
+    date: String
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -14,7 +21,8 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
+  // getStaticPaths가 하는 역할은 [id]에 올 수 있는 모든 옵션을 배열로 제공하는 것.
   const paths = getAllPostsIds();
   return {
     paths,
@@ -22,8 +30,9 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = getPostData(params.id);
+
   return {
     props: {
       postData
